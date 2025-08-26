b4485b583f61d33859a53476c6ba5ef3671a230f
ScriptSmith70 <insidestory100@gmail.com>
2025-08-26 22:51:47 +0530

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -1,18 +1,23 @@
 import java.util.*;
-public class multiply{
-    public static void main(String[] args){
-        product();
 
+public class Multiply {
+    public static void main(String[] args) {
+        int result = product(); 
+        System.out.println("Product: " + result); 
     }
-    static void product(){
-        int num1, num2, product1;
+
+    static int product() {
         Scanner sc = new Scanner(System.in);
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
-        product1 = num1*num2;
-        System.out.println(product1);
+        
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        return num1*num2;
+        
         sc.close();
 
-
+       
     }
 }
