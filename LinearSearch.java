cd22d6bcf9b906be7d24f0286f1427dc1de5086b
ScriptSmith70 <insidestory100@gmail.com>
2025-08-27 09:06:59 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch{
+    static int search(int arr[],int target){
+        if(arr.length == 0) return -1;
+        for (int i =0; i<arr.length; i++){
+            if(arr[i] == target)
+            return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int arr[] = {12,34,56,78,13,53};
+        int ans = search(arr, 56);
+        System.out.println(ans);
+    
+}
+}
